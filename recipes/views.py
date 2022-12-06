65c1cf0db60655203cf6d8e31added479641292e
Nul0 <danielpedro7676@gmail.com>
2022-12-06 18:58:28 -0300

    final django models and ORM

--- recipes/views.py
+++ recipes/views.py
@@ -1,16 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import get_list_or_404, get_object_or_404, render
 
-from utils.recipes.factory import make_recipe
+from . import models
 
 
 def home(request):
+    recipes = models.Recipe.objects.filter(is_published=True).order_by('-id')
     return render(request, 'recipes/pages/home.html', context={
-        'recipes': [make_recipe() for _ in range(10)],
+        'recipes': recipes,
+    })
+
+
+def category(request, category_id):
+    recipes = get_list_or_404(
+        models.Recipe.objects.filter(
+            category__id=category_id, is_published=True)
+        .order_by('-id')
+    )
+
+    return render(request, 'recipes/pages/category.html', context={
+        'recipes': recipes,
+        'title': f'{recipes[0].category.name} - Category | '
     })
 
 
 def recipe(request, id):
+
+    recipe = get_object_or_404(
+        models.Recipe.objects.filter(
+            id=id, is_published=True)
+    )
+
     return render(request, 'recipes/pages/recipeView.html', context={
-        'recipe': make_recipe(),
+        'recipe': recipe,
         'isDetailPage': True,
     })
